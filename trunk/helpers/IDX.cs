fef0d4e99ebd916bde2a6416e184fe4b43638ea9
pospeselr@gmail.com <pospeselr@gmail.com@644a0db3-4894-4123-13b7-6e4b71424918>
2012-09-07 21:07:00 +0000

    Initial commit of C# IDX and RBM loading classes

---
+++ trunk/helpers/IDX.cs
@@ -0,0 +1,652 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+public enum Endianness : ushort
+{
+	BigEndian = 0x0000,
+	LittleEndian = 0xFFFF
+}
+
+public enum DataFormat : byte
+{
+	UInt8 = 0x08,
+	SInt8 = 0x09,
+	SInt16 = 0x0B,
+	SInt32 = 0x0C,
+	Single = 0x0D,
+	Double = 0x0E
+}
+
+public class IDX
+{
+	// file data
+	uint _rows;
+	uint _row_length;
+	Endianness _idx_endianness;
+	static Endianness _system_endianness = BitConverter.IsLittleEndian ? Endianness.LittleEndian : Endianness.BigEndian;
+	DataFormat _data_format;
+
+	// bookkeeping
+	Stream _idx_stream;
+	uint _header_size;
+	uint _row_length_bytes;
+	bool _writing;
+
+	#region READS
+
+	byte ReadUInt8()
+	{
+		int result = _idx_stream.ReadByte();
+		Debug.Assert(result >= 0 && result <= 255);
+		return (byte)result;
+	}
+
+	unsafe uint ReadUInt32()
+	{
+		const int ALLOC = 4;
+
+		byte* buff = stackalloc byte[ALLOC];
+
+		if (_idx_endianness == _system_endianness)
+		{
+			buff[0] = ReadUInt8();
+			buff[1] = ReadUInt8();
+			buff[2] = ReadUInt8();
+			buff[3] = ReadUInt8();
+		}
+		else
+		{
+			buff[3] = ReadUInt8();
+			buff[2] = ReadUInt8();
+			buff[1] = ReadUInt8();
+			buff[0] = ReadUInt8();
+		}
+
+		return *(uint*)buff;
+	}
+
+
+	#endregion
+	
+	#region WRITES
+
+	void WriteUInt8(byte b)
+	{
+		_idx_stream.WriteByte(b);
+	}
+
+	unsafe void WriteUInt32(uint i)
+	{
+		byte* buff = (byte*)&i;
+
+		if (_idx_endianness == _system_endianness)
+		{
+			_idx_stream.WriteByte(buff[0]);
+			_idx_stream.WriteByte(buff[1]);
+			_idx_stream.WriteByte(buff[2]);
+			_idx_stream.WriteByte(buff[3]);
+		}
+		else
+		{
+			_idx_stream.WriteByte(buff[3]);
+			_idx_stream.WriteByte(buff[2]);
+			_idx_stream.WriteByte(buff[1]);
+			_idx_stream.WriteByte(buff[0]);		
+		}
+	}
+
+	#endregion
+
+	// constructor for reading an IDX file 
+	public IDX(Stream in_stream, bool in_writing=false)
+	{
+		_idx_stream = in_stream;
+		// verify we can actually use this stream
+		Debug.Assert(_idx_stream.CanRead && _idx_stream.CanSeek);
+
+		_writing = in_writing;
+		if (_writing)
+		{
+			// just verify stream is writeable
+			Debug.Assert(_idx_stream.CanWrite == true);
+		}
+
+
+		// get our endianness
+		_idx_endianness = (Endianness)(255 * ReadUInt8() + ReadUInt8());
+		Debug.Assert(_idx_endianness == Endianness.BigEndian || _idx_endianness == Endianness.LittleEndian);
+
+		// get data format
+		_data_format = (DataFormat)ReadUInt8();
+
+		// number of dimensions
+		byte dimensions = ReadUInt8();
+		Debug.Assert(dimensions > 0);
+		// get number of rows
+		_rows = ReadUInt32();
+
+		// calculate length of each row
+		_row_length = 1;
+		for (int k = 1; k < dimensions; k++)
+		{
+			_row_length *= ReadUInt32();
+		}
+
+		_row_length_bytes = _row_length;
+		switch (_data_format)
+		{
+			case DataFormat.UInt8:
+			case DataFormat.SInt8:
+				_row_length_bytes *= 1;
+				break;
+			case DataFormat.SInt16:
+				_row_length_bytes *= 2;
+				break;
+			case DataFormat.SInt32:
+			case DataFormat.Single:
+				_row_length_bytes *= 4;
+				break;
+			case DataFormat.Double:
+				_row_length_bytes *= 8;
+				break;
+		}
+
+		// get size of the header
+		_header_size = (uint)_idx_stream.Position;
+	}
+
+	// constructor for writing IDX objects
+	public IDX(Stream in_stream, Endianness in_endianness, DataFormat in_format, params uint[] row_dimensions)
+	{
+		_writing = true;
+		_idx_stream = in_stream;
+		// debug we can use this stream
+		Debug.Assert(_idx_stream.CanWrite && _idx_stream.CanRead && _idx_stream.CanSeek);
+
+		// set endianness
+		_idx_endianness = in_endianness;
+		// make sure valid endianness
+		Debug.Assert(_idx_endianness == Endianness.BigEndian || _idx_endianness == Endianness.LittleEndian);
+
+		// get the data format
+		_data_format = in_format;
+		// make sure it's a valid format
+		Debug.Assert(_data_format == DataFormat.UInt8 ||
+			_data_format == DataFormat.SInt8 ||
+			_data_format == DataFormat.SInt16 ||
+			_data_format == DataFormat.SInt32 ||
+			_data_format == DataFormat.Single ||
+			_data_format == DataFormat.Double);
+
+		// verify row dimensions are all valid
+		Debug.Assert(row_dimensions.Length > 1 && row_dimensions.Length < 255);
+		_row_length = 1;
+		for (int i = 0; i < row_dimensions.Length; i++)
+		{
+			_row_length *= row_dimensions[i];
+			Debug.Assert(row_dimensions[i] > 0);
+		}
+
+		_row_length_bytes = _row_length;
+		switch (_data_format)
+		{
+			case DataFormat.UInt8:
+			case DataFormat.SInt8:
+				_row_length_bytes *= 1;
+				break;
+			case DataFormat.SInt16:
+				_row_length_bytes *= 2;
+				break;
+			case DataFormat.SInt32:
+			case DataFormat.Single:
+				_row_length_bytes *= 4;
+				break;
+			case DataFormat.Double:
+				_row_length_bytes *= 8;
+				break;
+		}
+
+		// write out IDX header
+
+		// endianness first
+		if (_idx_endianness == Endianness.BigEndian)
+		{
+			WriteUInt8(0x00);
+			WriteUInt8(0x00);
+		}
+		else
+		{
+			WriteUInt8(0xFF);
+			WriteUInt8(0xFF);
+		}
+
+		// write data format
+		WriteUInt8((byte)_data_format);
+		// write number of dimensions
+		WriteUInt8((byte)(1 + row_dimensions.Length));
+		// number of rows is 0 until we actually add rows
+		WriteUInt32(0);
+		foreach (uint i in row_dimensions)
+		{
+			WriteUInt32(i);
+		}
+
+		_header_size = (uint)_idx_stream.Position;
+	}
+
+
+
+	public uint Rows
+	{
+		get
+		{
+			return _rows;
+		}
+
+	}
+
+	public uint RowLength
+	{
+		get
+		{
+			return _row_length;
+		}
+	}
+
+	public DataFormat DataType
+	{
+		get
+		{
+			return _data_format;
+		}
+	}
+
+	public uint AddRow()
+	{
+		Debug.Assert(_writing == true);		// can only add rows when we're writing
+		Debug.Assert(_rows != uint.MaxValue);	// prevent overflow
+
+		_idx_stream.Position = _header_size + _row_length_bytes * _rows;
+		for (uint i = 0; i < _row_length_bytes; i++)
+		{
+			_idx_stream.WriteByte((byte)0);
+		}
+
+		return _rows++;	// returns index of new row
+	}
+
+	public void AddRows(uint count)
+	{
+		Debug.Assert(_writing == true);	// can only add rows when writing
+		Debug.Assert(count != 0);	// verify count is positive
+		Debug.Assert(_rows + count > _rows);	// make sure we don't overflow
+
+		_idx_stream.Position = _header_size + _row_length_bytes * _rows;
+		for (uint k = 0; k < count; k++)
+		{
+			for (uint i = 0; i < _row_length; i++)
+			{
+				_idx_stream.WriteByte((byte)0);
+			}
+		}
+		_rows += count;
+	}
+	#region Row Reads
+
+	public void ReadRow(uint index, byte[] buffer)
+	{
+		Debug.Assert(index < _rows);
+		Debug.Assert(_data_format == DataFormat.UInt8);
+
+		_idx_stream.Position = _header_size + _row_length_bytes * index;
+
+		_idx_stream.Read(buffer, 0, (int)_row_length);
+	}
+
+	public void ReadRow(uint index, sbyte[] buffer)
+	{
+		Debug.Assert(index < _rows);
+		Debug.Assert(_data_format == DataFormat.SInt8);
+
+		_idx_stream.Position = _header_size + _row_length_bytes * index;
+
+		for (uint k = 0; k < _row_length; k++)
+		{
+			buffer[k] = (sbyte)ReadUInt8();
+		}
+	}
+
+	unsafe public void ReadRow(uint index, short[] buffer)
+	{
+		Debug.Assert(index < _rows);
+		Debug.Assert(_data_format == DataFormat.SInt16);
+
+		_idx_stream.Position = _header_size + _row_length_bytes * index;
+
+		byte* temp = stackalloc byte[2];
+		short* val = (short*)temp;
+
+		if (_idx_endianness == _system_endianness)
+		{
+			for (uint k = 0; k < _row_length; k++)
+			{
+				temp[0] = ReadUInt8();
+				temp[1] = ReadUInt8();
+
+				buffer[k] = *val;
+			}
+		}
+		else
+		{
+			for (uint k = 0; k < _row_length; k++)
+			{
+				temp[1] = ReadUInt8();
+				temp[0] = ReadUInt8();
+
+				buffer[k] = *val;
+			}
+		}
+	}
+
+	unsafe public void ReadRow(uint index, int[] buffer)
+	{
+		Debug.Assert(index < _rows);
+		Debug.Assert(_data_format == DataFormat.SInt32);
+
+		_idx_stream.Position = _header_size + _row_length_bytes * index;
+
+		byte* temp = stackalloc byte[4];
+		int* val = (int*)temp;
+
+		if (_idx_endianness == _system_endianness)
+		{
+			for (uint k = 0; k < _row_length; k++)
+			{
+				temp[0] = ReadUInt8();
+				temp[1] = ReadUInt8();
+				temp[2] = ReadUInt8();
+				temp[3] = ReadUInt8();
+
+				buffer[k] = *val;
+			}
+		}
+		else
+		{
+			for (uint k = 0; k < _row_length; k++)
+			{
+				temp[3] = ReadUInt8();
+				temp[2] = ReadUInt8();
+				temp[1] = ReadUInt8();
+				temp[0] = ReadUInt8();
+
+				buffer[k] = *val;
+			}
+		}
+	}
+
+	unsafe public void ReadRow(uint index, float[] buffer)
+	{
+		Debug.Assert(index < _rows);
+		Debug.Assert(_data_format == DataFormat.Single);
+
+		_idx_stream.Position = _header_size + _row_length_bytes * index;
+
+		byte* temp = stackalloc byte[4];
+		float* val = (float*)temp;
+
+		if (_idx_endianness == _system_endianness)
+		{
+			for (uint k = 0; k < _row_length; k++)
+			{
+				temp[0] = ReadUInt8();
+				temp[1] = ReadUInt8();
+				temp[2] = ReadUInt8();
+				temp[3] = ReadUInt8();
+
+				buffer[k] = *val;
+			}
+		}
+		else
+		{
+			for (uint k = 0; k < _row_length; k++)
+			{
+				temp[3] = ReadUInt8();
+				temp[2] = ReadUInt8();
+				temp[1] = ReadUInt8();
+				temp[0] = ReadUInt8();
+
+				buffer[k] = *val;
+			}
+		}
+	}
+
+	unsafe public void ReadRow(uint index, double[] buffer)
+	{
+		Debug.Assert(index < _rows);
+		Debug.Assert(_data_format == DataFormat.Double);
+		
+		_idx_stream.Position = _header_size + _row_length_bytes * index;
+
+		byte* temp = stackalloc byte[4];
+		float* val = (float*)temp;
+
+		if (_idx_endianness == _system_endianness)
+		{
+			for (uint k = 0; k < _row_length; k++)
+			{
+				temp[0] = ReadUInt8();
+				temp[1] = ReadUInt8();
+				temp[2] = ReadUInt8();
+				temp[3] = ReadUInt8();
+				temp[4] = ReadUInt8();
+				temp[5] = ReadUInt8();
+				temp[6] = ReadUInt8();
+				temp[7] = ReadUInt8();
+
+				buffer[k] = *val;
+			}
+		}
+		else
+		{
+			for (uint k = 0; k < _row_length; k++)
+			{
+				temp[7] = ReadUInt8();
+				temp[6] = ReadUInt8();
+				temp[5] = ReadUInt8();
+				temp[4] = ReadUInt8();
+				temp[3] = ReadUInt8();
+				temp[2] = ReadUInt8();
+				temp[1] = ReadUInt8();
+				temp[0] = ReadUInt8();
+
+				buffer[k] = *val;
+			}
+		}
+	}
+
+	#endregion
+
+	#region Row Writes
+
+	public void WriteRow(uint index, byte[] buffer)
+	{
+		Debug.Assert(buffer.Length == _row_length);	// right amount of data
+		Debug.Assert(_data_format == DataFormat.UInt8);	// right data format
+		Debug.Assert(index < _rows);	// valid index
+		Debug.Assert(_writing == true);	// make sure we can actually write
+
+		_idx_stream.Position = _header_size + _rows * _row_length_bytes;
+
+		_idx_stream.Write(buffer, 0, (int)_row_length);
+		_rows++;
+	}
+
+	unsafe public void WriteRow(int index, sbyte[] in_buffer)
+	{
+		Debug.Assert(in_buffer.Length == _row_length);	// right amount of data
+		Debug.Assert(_data_format == DataFormat.SInt8);	// right data format
+		Debug.Assert(index < _rows);	// valid index
+		Debug.Assert(_writing == true);	// make sure we can actually write
+
+		_idx_stream.Position = _header_size + _row_length_bytes * index;
+
+		fixed(sbyte* buffer = in_buffer)
+		{
+			byte* raw = (byte*)buffer;
+			for (int k = 0; k < _row_length; k++)
+			{
+				WriteUInt8(raw[k]);
+			}
+		}
+	}
+
+	unsafe public void WriteRow(uint index, short[] in_buffer)
+	{
+		Debug.Assert(in_buffer.Length == _row_length);	// right amount of data
+		Debug.Assert(_data_format == DataFormat.SInt16);	// right data format
+		Debug.Assert(index < _rows);	// valid index
+		Debug.Assert(_writing == true);	// make sure we can actually write
+
+		fixed(short* buffer = in_buffer)
+		{
+			byte* raw = (byte*)buffer;
+
+			_idx_stream.Position = _header_size + _row_length_bytes * index;
+
+			if (_idx_endianness == _system_endianness)
+			{
+				for (int k = 0; k < _row_length_bytes; k++)
+				{
+					WriteUInt8(raw[k]);
+				}
+			}
+			// byteswap
+			else
+			{
+				for (int k = 0; k < _row_length; k++)
+				{
+					WriteUInt8(raw[1]);
+					WriteUInt8(raw[0]);
+					raw += 2;
+				}
+			}
+		}
+	}
+
+	unsafe public void WriteRow(uint index, int[] in_buffer)
+	{
+		Debug.Assert(in_buffer.Length == _row_length);	// right amount of data
+		Debug.Assert(_data_format == DataFormat.SInt32);	// right data format
+		Debug.Assert(index < _rows);	// valid index
+		Debug.Assert(_writing == true);	// make sure we can actually write
+
+		fixed (int* buffer = in_buffer)
+		{
+			byte* raw = (byte*)buffer;
+
+			_idx_stream.Position = _header_size + _row_length_bytes * index;
+
+			if (_idx_endianness == _system_endianness)
+			{
+				for (int k = 0; k < _row_length_bytes; k++)
+				{
+					WriteUInt8(raw[k]);
+				}
+			}
+			// byteswap
+			else
+			{
+				for (int k = 0; k < _row_length; k++)
+				{
+					WriteUInt8(raw[3]);
+					WriteUInt8(raw[2]);
+					WriteUInt8(raw[1]);
+					WriteUInt8(raw[0]);
+					raw += 4;
+				}
+			}
+		}
+	}
+
+	unsafe public void WriteRow(uint index, float[] in_buffer)
+	{
+		Debug.Assert(in_buffer.Length == _row_length);	// right amount of data
+		Debug.Assert(_data_format == DataFormat.Single);	// right data format
+		Debug.Assert(index < _rows);	// valid index
+		Debug.Assert(_writing == true);	// make sure we can actually write
+
+		fixed (float* buffer = in_buffer)
+		{
+			byte* raw = (byte*)buffer;
+
+			_idx_stream.Position = _header_size + _row_length_bytes * index;
+
+			if (_idx_endianness == _system_endianness)
+			{
+				for (int k = 0; k < _row_length_bytes; k++)
+				{
+					WriteUInt8(raw[k]);
+				}
+			}
+			// byteswap
+			else
+			{
+				for (int k = 0; k < _row_length; k++)
+				{
+					WriteUInt8(raw[3]);
+					WriteUInt8(raw[2]);
+					WriteUInt8(raw[1]);
+					WriteUInt8(raw[0]);
+					raw += 4;
+				}
+			}
+		}
+
+		_rows++;
+	}
+
+	unsafe public void WriteRow(uint index, double[] in_buffer)
+	{
+		Debug.Assert(in_buffer.Length == _row_length); // right amount of data
+		Debug.Assert(_data_format == DataFormat.Double);	// right data format
+		Debug.Assert(index < _rows);	// valid index
+		Debug.Assert(_writing == true);	// make sure we can actually write
+
+		fixed (double* buffer = in_buffer)
+		{
+			byte* raw = (byte*)buffer;
+
+			_idx_stream.Position = _header_size + _row_length_bytes * index;
+
+			if (_idx_endianness == _system_endianness)
+			{
+				for (int k = 0; k < _row_length_bytes; k++)
+				{
+					WriteUInt8(raw[k]);
+				}
+			}
+			// byteswap
+			else
+			{
+				for (int k = 0; k < _row_length; k++)
+				{
+					WriteUInt8(raw[7]);
+					WriteUInt8(raw[6]);
+					WriteUInt8(raw[5]);
+					WriteUInt8(raw[4]);
+					WriteUInt8(raw[3]);
+					WriteUInt8(raw[2]);
+					WriteUInt8(raw[1]);
+					WriteUInt8(raw[0]);
+					raw += 8;
+				}
+			}
+		}
+	}
+
+#endregion
+}
---
+++ trunk/helpers/RBM.cs
@@ -0,0 +1,423 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+public enum UnitType : byte
+{
+	Sigmoid = 0x00,
+	Linear = 0xFF
+}
+
+unsafe class Memory
+{
+    public static void* malloc(Int32 size)
+    {
+        IntPtr ip = Marshal.AllocHGlobal(size);
+        return ip.ToPointer();
+    }
+
+    public static void free(void* ptr)
+    {
+        Debug.Assert(ptr != null);
+        Marshal.FreeHGlobal(new IntPtr(ptr));
+    }
+}
+
+unsafe public class Sigmoid
+{
+	const float _max = 15.0f;
+	const float _min = -_max;
+
+	const int _count = 2048;
+	const float _scale = (_count - 1) / _max;
+
+    static float* _pos_vals;
+    static float* _neg_vals;
+
+	static Sigmoid()
+	{
+        _pos_vals = (float*)Memory.malloc(sizeof(float) * _count);
+        _neg_vals = (float*)Memory.malloc(sizeof(float) * _count);
+
+        const double _dscale = ((_count - 1) / (double)_max);
+
+		for (int k = 0; k < _count; k++)
+		{
+			double x = k;
+			_pos_vals[k] = (float)(1.0 / (1.0 + Math.Exp(-x / _dscale)));
+			_neg_vals[k] = (float)(1.0 / (1.0 + Math.Exp(x / _dscale)));
+		}
+	}
+
+	public static float Calc(float x)
+	{
+		if (x <= _min) return 0.0f;
+		if (x >= _max) return 1.0f;
+
+		float result = 0.5f;
+
+		if (x > 0.0f)
+		{
+			result = _pos_vals[(int)(x * _scale + 0.5f)];
+		}
+		else if (x < 0.0f)
+		{
+			result = _neg_vals[(int)(-x * _scale + 0.5f)];
+		}
+
+		return result;
+	}
+
+	~Sigmoid()
+	{
+        Memory.free(_pos_vals);
+        Memory.free(_neg_vals);
+	}
+}
+
+// use same parmeters as Sigmoid since this is actually the integral of Sigmoid
+unsafe public class LnOnePlusExp
+{
+	const float _max = 15.0f;
+	const float _min = -_max;
+
+	const int _count = 2048;
+	const float _scale = (_count - 1) / _max;
+
+	static float* _pos_vals;
+	static float* _neg_vals;
+
+	static LnOnePlusExp()
+	{
+		_pos_vals = (float*)Memory.malloc(sizeof(float) * _count);
+		_neg_vals = (float*)Memory.malloc(sizeof(float) * _count);
+
+		const double _dscale = ((_count - 1) / (double)_max);
+
+		for (int k = 0; k < _count; k++)
+		{
+			double x = k;
+			_pos_vals[k] = (float)Math.Log(1.0 + Math.Exp(x / _dscale));
+			_neg_vals[k] = (float)Math.Log(1.0 + Math.Exp(-x / _dscale));
+		}
+	}
+
+	public static float Calc(float x)
+	{
+		if (x <= _min) return 0.0f;
+		if (x >= _max) return x;
+
+		// ln(1 + e^x)  == ln(2)
+		float result = 0.6931471805599453f;
+
+		if (x > 0.0f)
+		{
+			result = _pos_vals[(int)(x * _scale + 0.5f)];	
+		}
+		else if (x < 0.0f)
+		{
+			result = _neg_vals[(int)(-x * _scale + 0.5f)];
+		}
+
+		return result;
+	}
+
+	~LnOnePlusExp()
+	{
+		Memory.free(_pos_vals);
+		Memory.free(_neg_vals);
+	}
+}
+
+unsafe public class RBM
+{
+	static Endianness _system_endianness = BitConverter.IsLittleEndian ? Endianness.LittleEndian : Endianness.BigEndian;
+
+	ushort _visible;
+	ushort _hidden;
+
+	UnitType _visible_type;
+
+    float* _visible_means;
+    float* _visible_stddevs;
+
+    float* _visible_biases;
+    float* _hidden_biases;
+
+    float** _visible_features;
+    float** _hidden_features;
+
+    uint* _random;
+
+	Stream _rbm_stream;
+
+	#region READS
+
+	byte ReadUInt8()
+	{
+		int result = _rbm_stream.ReadByte();
+		Debug.Assert(result >= 0 && result <= 255);
+		return (byte)result;
+	}
+
+	unsafe ushort ReadUInt16()
+	{
+		byte* buff = stackalloc byte[2];
+		ushort* result = (ushort*)buff;
+
+		if (_system_endianness == Endianness.LittleEndian)
+		{
+			buff[0] = ReadUInt8();
+			buff[1] = ReadUInt8();
+		}
+		else
+		{
+			buff[1] = ReadUInt8();
+			buff[0] = ReadUInt8();
+		}
+
+		return *result;
+	}
+
+	unsafe void ReadSingle(float* dest, int count)
+	{
+		byte* buff = stackalloc byte[2];
+		float* result = (float*)buff;
+
+		if (_system_endianness == Endianness.LittleEndian)
+		{
+            for (int k = 0; k < count; k++)
+			{
+				buff[0] = ReadUInt8();
+				buff[1] = ReadUInt8();
+				buff[2] = ReadUInt8();
+				buff[3] = ReadUInt8();
+
+				dest[k] = *result;
+			}
+		}
+		else
+		{
+            for (int k = 0; k < count; k++)
+			{
+				buff[3] = ReadUInt8();
+				buff[2] = ReadUInt8();
+				buff[1] = ReadUInt8();
+				buff[0] = ReadUInt8();
+
+				dest[k] = *result;
+			}
+		}
+	}
+
+	#endregion
+
+	private RBM()
+	{
+
+	}
+
+    ~RBM()
+    {
+        if (_visible_type == UnitType.Linear)
+        {
+            Memory.free(_visible_means);
+            Memory.free(_visible_stddevs);
+        }
+
+        Memory.free(_visible_biases);
+        Memory.free(_hidden_biases);
+
+        for (int i = 0; i < _visible; i++)
+        {
+            Memory.free(_hidden_features[i]);
+        }
+        Memory.free(_hidden_features);
+
+        for(int j = 0; j < _hidden; j++)
+        {
+            Memory.free(_visible_features[j]);
+        }
+        Memory.free(_visible_features);
+
+        Memory.free(_random);
+    }
+
+	public static RBM Load(Stream in_stream)
+	{
+		RBM result = new RBM();
+		result._rbm_stream = in_stream;
+
+		Debug.Assert(result.ReadUInt8() == '.');
+		Debug.Assert(result.ReadUInt8() == 'R');
+		Debug.Assert(result.ReadUInt8() == 'B');
+		Debug.Assert(result.ReadUInt8() == 'M');
+
+		// get the visible type
+		result._visible_type = (UnitType)result.ReadUInt8();
+		// read counts
+		result._visible = result.ReadUInt16();
+		result._hidden = result.ReadUInt16();
+		// read stats
+		if (result._visible_type == UnitType.Linear)
+		{
+            result._visible_means = (float*)Memory.malloc(sizeof(float) * result._visible);
+			result.ReadSingle(result._visible_means, result._visible);
+            result._visible_stddevs = (float*)Memory.malloc(sizeof(float) * result._visible);
+			result.ReadSingle(result._visible_stddevs, result._visible);
+		}
+		// read biases
+        result._visible_biases = (float*)Memory.malloc(sizeof(float) * result._visible);
+		result.ReadSingle(result._visible_biases, result._visible);
+        result._hidden_biases = (float*)Memory.malloc(sizeof(float) * result._hidden);
+		result.ReadSingle(result._hidden_biases, result._hidden);
+
+		// get the weights weights
+
+        result._hidden_features = (float**)Memory.malloc(sizeof(float*) * result._visible);
+		for (uint i = 0; i < result._visible; i++)
+		{
+			result._hidden_features[i] = (float*)Memory.malloc(sizeof(float) * result._hidden);
+			result.ReadSingle(result._hidden_features[i], result._hidden);
+		}
+
+		// now transpose
+		result._visible_features = (float**)Memory.malloc(sizeof(float*) * result._hidden);
+		for (uint j = 0; j < result._hidden; j++)
+		{
+			result._visible_features[j] = (float*)Memory.malloc(sizeof(float) * result._visible);
+			for (uint i = 0; i < result._visible; i++)
+			{
+				result._visible_features[j][i] = result._hidden_features[i][j];
+			}
+		}
+
+        // get seeds for our random buffer
+        result._random = (uint*)Memory.malloc(sizeof(int) * result._hidden);
+        Random rand = new Random();
+        for (int j = 0; j < result._hidden; j++)
+        {
+            result._random[j] = (uint)rand.Next();
+        }
+
+		return result;
+	}
+
+	public void CalcHiddenActivations(float[] in_visible_vector, float[] out_hidden)
+	{
+        // figure out how to fix these vectors
+
+		Debug.Assert(in_visible_vector.Length == _visible);
+		Debug.Assert(out_hidden.Length == _hidden);
+
+		fixed (float* v = in_visible_vector, h = out_hidden)
+		{
+			for (uint j = 0; j < _hidden; j++)
+			{
+				h[j] = _hidden_biases[j];
+				for (uint i = 0; i < _visible; i++)
+				{
+					h[j] += _visible_features[j][i] * v[i];
+				}
+			}
+		}
+	}
+
+	public void CalcHiddenProbabilities(float[] in_visible_vector, float[] out_hidden)
+	{
+		fixed (float* v = in_visible_vector, h = out_hidden)
+		{
+			for (uint j = 0; j < _hidden; j++)
+			{
+				h[j] = _hidden_biases[j];
+				for (uint i = 0; i < _visible; i++)
+				{
+					h[j] += _visible_features[j][i] * v[i];
+				}
+				h[j] = Sigmoid.Calc(h[j]);
+			}
+		}
+    }
+
+	public void CalcHiddenStates(float[] in_visible_vector, float[] out_hidden)
+	{
+		// val for our prng
+		// http://en.wikipedia.org/wiki/Linear_congruential_generator (Numerical Recipes vals)
+        const uint a = 1664525;
+        const uint c = 1013904223;
+
+		fixed (float* v = in_visible_vector, h = out_hidden)
+		{
+			for (uint j = 0; j < _hidden; j++)
+			{
+				// activation
+				h[j] = _hidden_biases[j];
+				for (uint i = 0; i < _visible; i++)
+				{
+					h[j] += _visible_features[j][i] * v[i];
+				}
+				// probability
+				h[j] = Sigmoid.Calc(h[j]);
+				_random[j] = _random[j] * a + c;
+				// state
+				h[j] = (_random[j] < h[j] * uint.MaxValue) ? 1.0f : 0.0f;
+			}
+		}
+	}
+
+    public void CalcVisible(float[] in_hidden, float[] out_visible)
+    {
+		fixed (float* h = in_hidden, v = out_visible)
+		{
+			for (int i = 0; i < _visible; i++)
+			{
+				v[i] = _visible_biases[i];
+				for (int j = 0; j < _hidden; j++)
+				{
+					v[i] += _hidden_features[i][j] * h[j];
+				}
+			}
+
+			if (_visible_type == UnitType.Sigmoid)
+			{
+				for (int i = 0; i < _visible; i++)
+				{
+					v[i] = Sigmoid.Calc(v[i]);
+				}
+			}
+		}
+    }
+
+    public float CalcFreeEnergy(float[] in_visible)
+    {
+        // equation 25 in Practical Guide
+
+		float F_v = 0.0f;
+
+		fixed (float* v = in_visible)
+		{
+			for (int i = 0; i < _visible; i++)
+			{
+				F_v -= _visible_biases[i] * v[i];
+			}
+
+			for (int j = 0; j < _hidden; j++)
+			{
+				float x_j = _hidden_biases[j];
+				for (int i = 0; i < _visible; i++)
+				{
+					x_j += _visible_features[j][i] * v[i];
+				}
+				
+				// lookup table similar to Sigmoid
+				F_v -= LnOnePlusExp.Calc(x_j);
+			}
+		}
+		return F_v;
+    }
+}
+
