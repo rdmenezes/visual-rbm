ecfb4214b1cf1e67e5e4ac386657450e06bb1ff4
pospeselr@gmail.com <pospeselr@gmail.com@644a0db3-4894-4123-13b7-6e4b71424918>
2012-11-19 03:23:11 +0000

    Finished catidx program, updated joinidx's code flow and error messaging to match catidx

--- source/Tools/joinidx/joinidx.cpp
+++ source/Tools/joinidx/joinidx.cpp
@@ -5,34 +5,43 @@
 const char* Usage = 
 	"Joins together multiple IDX files with the same number of rows into a\n"
 	"single IDX file.\n"
-
+	"\n"
 	"Usage: joinidx [INPUTS] [OUTPUT]\n"
 	"  INPUTS      A list of 1 or more IDX files\n"
 	"  OUTPUT      Output joined IDX file\n";
 
 int main(int argc, char** argv)
 {
-	if(argc < 3)
+	int result = -1;
+
+	if(argc < 4)
 	{
 		printf(Usage);
-		return -1;
+		return result;
 	}
 
 	uint32_t idx_count = argc - 2;
-
 	IDX** inputs = new IDX*[idx_count];
+	memset(inputs, NULL, sizeof(IDX*) * idx_count);
 	DataFormat data_format;
 	uint32_t rows;
 	uint32_t row_length = 0;
+	// our output IDX file
+	IDX* output = NULL;
+	// temp buffer for reading/writing rows
+	uint8_t* row_buffer = NULL;
 
 	// iterate through each idx file, and verify data is consistent
 	for(uint32_t k = 0; k < idx_count; k++)
 	{
-		IDX* idx = IDX::Load(argv[k + 1]);
+		const char* idx_filename = argv[k+1];
+		IDX* idx = IDX::Load(idx_filename);
+		inputs[k] = idx;
+
 		if(idx == NULL)
 		{
-			printf("Unable to open IDX file %s\n", argv[k + 1]);
-			return -1;
+			printf("Unable to open IDX file \"%s\" for reading\n", idx_filename);
+			goto CLEANUP;
 		}
 
 
@@ -45,17 +54,17 @@ int main(int argc, char** argv)
 		{
 			if(data_format != idx->GetDataFormat())
 			{
-				printf("Data type in %s is inconsistent with the other files\n", argv[k + 1]);
-				return -1;
+				printf("Data type in %s is inconsistent with the other files\n", idx_filename);
+				goto CLEANUP;
 			}
 			else if(rows != idx->GetRowCount())
 			{
-				printf("Number of rows in %s is inconsistent with the other files\n", argv[k + 1]);
-				return -1;
+				printf("Number of rows in %s is inconsistent with the other files\n", idx_filename);
+				goto CLEANUP;
 			}
 		}
+
 		row_length += idx->GetRowLength();
-		inputs[k] = idx;
 	}
 
 	// get the number of bytes per row
@@ -78,17 +87,18 @@ int main(int argc, char** argv)
 		break;
 	}
 	
-	uint8_t* row_buffer = new uint8_t[row_length * data_width];
-
+	const char* output_filename = argv[argc - 1];
 	// last filename is the result
-	IDX* output = IDX::Create(argv[argc - 1], LittleEndian, data_format, row_length);
+	output = IDX::Create(output_filename, LittleEndian, data_format, row_length);
 	if(output == NULL)
 	{
-		printf("Unable to create output file %s\n", argv[argc - 1]);
-		return -1;
+		printf("Unable to create output file %s\n", output_filename);
+		goto CLEANUP;
 	}
 
-	printf("Writing %s to disk ... \n", argv[argc-1]);
+	row_buffer = (uint8_t*)malloc(output->GetRowLengthBytes());
+
+	printf("Writing %s to disk ... \n", output_filename);
 
 	for(uint32_t k = 0; k < rows; k++)
 	{
@@ -103,15 +113,30 @@ int main(int argc, char** argv)
 	// write out header
 	output->Close();
 
-	// cleanup
-	delete output;
+	printf("Done!\n");
+
+	// success!
+	result = 0;
+
+CLEANUP:
+
 	for(uint32_t i = 0; i < idx_count; i++)
 	{
-		delete inputs[i];
+		if(inputs[i])
+		{
+			delete inputs[i];
+		}
 	}
 	delete[] inputs;
 
-	printf("Done!\n");
+	if(output)
+	{
+		delete output;
+	}
+	if(row_buffer)
+	{
+		free(row_buffer);
+	}
 
-	return 0;
+	return result;
 }
