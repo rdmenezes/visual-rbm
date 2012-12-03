c3a600efc674e5dc0225611ce749399ccd1ebc85
pospeselr@gmail.com <pospeselr@gmail.com@644a0db3-4894-4123-13b7-6e4b71424918>
2012-12-03 05:32:07 +0000

    Updated the publish script to include splitidx and calchidden

--- release/publish.bat
+++ release/publish.bat
@@ -6,6 +6,6 @@ del VisualRBM.zip
 del Tools.zip
 
 %ZIP% a VisualRBM.zip %BIN%\VisualRBM.exe %BIN%\QuickBoltzmannInterop.dll %BIN%\NativeShaders\
-%ZIP% a Tools.zip %BIN%\clrbm.exe %BIN%\catidx.exe %BIN%\joinidx.exe %BIN%\idxinfo.exe
+%ZIP% a Tools.zip %BIN%\clrbm.exe %BIN%\catidx.exe %BIN%\joinidx.exe %BIN%\idxinfo.exe %BIN%\splitidx.exe %BIN%\calchidden.exe
 
 pause
