1ab786a1c981cc904dad7e8c95187f2ec07acc11
pospeselr@gmail.com <pospeselr@gmail.com@644a0db3-4894-4123-13b7-6e4b71424918>
2012-12-14 06:37:09 +0000

    Updated publish script, shader files no longer required

--- release/publish.bat
+++ release/publish.bat
@@ -1,11 +1,10 @@
 set BIN="..\bin\Release"
 set ZIP="C:\Program Files\7-Zip\7z.exe"
 
-
 del VisualRBM.zip
 del Tools.zip
 
-%ZIP% a VisualRBM.zip %BIN%\VisualRBM.exe %BIN%\QuickBoltzmannInterop.dll %BIN%\NativeShaders\
+%ZIP% a VisualRBM.zip %BIN%\VisualRBM.exe %BIN%\QuickBoltzmannInterop.dll
 %ZIP% a Tools.zip %BIN%\clrbm.exe %BIN%\catidx.exe %BIN%\joinidx.exe %BIN%\idxinfo.exe %BIN%\splitidx.exe %BIN%\calchidden.exe
 
 pause
