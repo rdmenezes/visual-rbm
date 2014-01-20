e582f194fbb55c4027b383b5f5450866df45f65a
pospeselr@gmail.com <pospeselr@gmail.com@644a0db3-4894-4123-13b7-6e4b71424918>
2014-01-20 00:42:09 +0000

    - refactored schedule to work better with VisualRBM's requirements

--- trunk/source/OMLT/OMLT/include/TrainingSchedule.h
+++ trunk/source/OMLT/OMLT/include/TrainingSchedule.h
@@ -17,7 +17,7 @@ namespace OMLT
 			: model_config(in_model_config)
 			, minibatch_size(in_minibatch_size)
 			, epochs_remaining(0)
-			, index(-1)
+			, index(0)
 		{
 			
 		}
@@ -27,32 +27,42 @@ namespace OMLT
 			train_config.push_back(std::pair<struct T::TrainingConfig, uint32_t>(in_train_config, in_epochs));
 		}
 
-		// returns true if we are done training
-		bool TrainingComplete() const
+		void StartTraining()
 		{
-			// no more training configs, and no more epochs
-			return index == (train_config.size()) && epochs_remaining == 0;
+			assert(train_config.size() > 0);
+			epochs_remaining = train_config.front().second;
+			index = 0;
 		}
 
-		// returns true if we have a new training config, false if not
-		bool NextEpoch(struct T::TrainingConfig& out_config)
+		bool TrainingComplete()
 		{
+			return index == train_config.size();
+		}
+
+		// returns true if we're done with the current parameters and need to get the next ones
+		bool NextEpoch()
+		{
+			assert(index <= train_config.size());
 			epochs_remaining = epochs_remaining == 0 ? 0 : epochs_remaining - 1;
+
 			if(epochs_remaining == 0)
 			{
 				index = std::min(index + 1, train_config.size());
-				if(index == train_config.size())
+				// verify we have epochs left
+				if((index) < train_config.size())
 				{
-					return false;
-				}
-				else
-				{
-					out_config = train_config[index].first;
 					epochs_remaining = train_config[index].second;
-					return true;
 				}
+				return true;
 			}
+
 			return false;
+		}
+
+		bool GetTrainingConfig(struct T::TrainingConfig& out_config)
+		{
+			out_config = train_config[index].first;
+			return true;
 		}
 
 		struct T::ModelConfig GetModelConfig() const
