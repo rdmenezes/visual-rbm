e93d709bee79ad42119ac9b306947999e3602e4b
pospeselr@gmail.com <pospeselr@gmail.com@644a0db3-4894-4123-13b7-6e4b71424918>
2014-02-13 05:20:47 +0000

    - updated the release script

--- trunk/release/publish.bat
+++ trunk/release/publish.bat
@@ -1,10 +1,35 @@
+:: directories
 set BIN="..\bin\Release"
+
+:: solutions
+set OMLT="..\source\OMLT\OMLT.sln"
+set VISUALRBM="..\source\VisualRBM\VisualRBM.sln"
+set TOOLS="..\source\Tools\Tools.sln"
+
+:: executables
 set ZIP="C:\Program Files\7-Zip\7z.exe"
+set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
+
+:: delete odl releases
 
 del VisualRBM.zip
 del Tools.zip
 
-%ZIP% a VisualRBM.zip %BIN%\VisualRBM.exe %BIN%\QuickBoltzmannInterop.dll
-%ZIP% a Tools.zip %BIN%\clrbm.exe %BIN%\catidx.exe %BIN%\joinidx.exe %BIN%\idxinfo.exe %BIN%\splitidx.exe %BIN%\calchidden.exe %BIN%\idx2csv.exe %BIN%\csv2idx.exe
+:: clean build
+
+%DEVENV% %OMLT% /Clean Release
+%DEVENV% %VISUALRBM% /Clean Release
+%DEVENV% %TOOLS% /Clean Release
+
+:: build releases
+
+%DEVENV% %OMLT% /Build Release
+%DEVENV% %VISUALRBM% /Build Release
+%DEVENV% %TOOLS% /Build Release
+
+:: create zips
+
+%ZIP% a VisualRBM.zip %BIN%\VisualRBM.exe %BIN%\VisualRBMInterop.dll
+%ZIP% a Tools.zip %BIN%\cltrain.exe %BIN%\catidx.exe %BIN%\joinidx.exe %BIN%\idxinfo.exe %BIN%\splitidx.exe %BIN%\calchidden.exe %BIN%\idx2csv.exe %BIN%\csv2idx.exe
 
 pause
