eda5db8df9a0f6abd1e0c6ee264b6b8d524996ab
pospeselr@gmail.com <pospeselr@gmail.com@644a0db3-4894-4123-13b7-6e4b71424918>
2014-05-03 17:39:36 +0000

    - refactored MLP related code to use FeatureMap rather than having raw pointers and SSE code everywhere

--- source/OMLT/OMLT/include/MultilayerPerceptron.h
+++ source/OMLT/OMLT/include/MultilayerPerceptron.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <vector>
 
+#include "Common.h"
 #include "Enums.h"
 #include "Model.h"
 
@@ -14,28 +15,24 @@ namespace OMLT
 	{
 	public:
 		~MultilayerPerceptron();
-		void FeedForward(float* input_vector, float* output_vector);
-		// feedfoward to the given layer
-		void FeedForward(float* input_vector, float* output_vector, uint32_t layer);
+		void FeedForward(float* input_vector, float* output_vector) const;
+		// feedforward to the given layer
+		void FeedForward(float* input_vector, float* output_vector, uint32_t layer) const;
 
 		struct Layer
 		{
 			uint32_t inputs;
 			uint32_t outputs;
 			ActivationFunction_t function;
-			float* biases;
-			float** weights;
+			FeatureMap weights;
 
 			Layer(uint32_t in_inputs, uint32_t in_outputs, ActivationFunction_t in_function);
-			~Layer();
 		};
 
 		Layer* GetLayer(uint32_t index);
 		Layer* InputLayer() {return GetLayer(0);}
 		Layer* OutputLayer() {return GetLayer(_layers.size() - 1);}
 
-		Layer* PopInputLayer();
-		Layer* PopOutputLayer();
 		bool AddLayer(Layer*);
 		uint32_t LayerCount() const {return _layers.size();}
 
@@ -43,13 +40,14 @@ namespace OMLT
 		std::string ToJSON() const;
 		static MultilayerPerceptron* FromJSON(const std::string& in_JSON);
 private: 
+		MultilayerPerceptron();
 		std::vector<Layer*> _layers;
 		// properly aligned scratch buffers
-		std::vector<float*> _accumulations;
-		std::vector<float*> _activations;
+		mutable std::vector<float*> _activations;
 		// private parse method
 		static MultilayerPerceptron* FromJSON(cJSON* root);
 		friend bool Model::FromJSON(const std::string& in_json, struct Model& out_model);
+		friend class BackPropagation;
 	};
 	typedef MultilayerPerceptron MLP;
 }
--- source/OMLT/OMLT/source/MultilayerPerceptron.cpp
+++ source/OMLT/OMLT/source/MultilayerPerceptron.cpp
@@ -14,8 +14,11 @@
 
 namespace OMLT
 {
-	extern __m128 _mm_rectifiedlinear_ps(__m128 x0);
-	extern __m128 _mm_sigmoid_ps(__m128 x0);
+	MultilayerPerceptron::MultilayerPerceptron()
+	{
+		_activations.push_back(nullptr);
+		_activations.push_back(nullptr);
+	}
 
 	MultilayerPerceptron::~MultilayerPerceptron()
 	{
@@ -24,116 +27,29 @@ namespace OMLT
 			delete _layers[k];
 		}
 
-		// offset index because first ptr is nullptr
-		for(uint32_t k = 1; k < _accumulations.size(); k++)
-		{
-			_aligned_free(_accumulations[k]);
-		}
-
 		for(uint32_t k = 0; k < _activations.size(); k++)
 		{
-			_aligned_free(_activations[k]);
+			AlignedFree(_activations[k]);
 		}
 	}
 
-	void MultilayerPerceptron::FeedForward(float* input_vector, float* output_vector)
+	void MultilayerPerceptron::FeedForward(float* input_vector, float* output_vector) const
 	{
 		FeedForward(input_vector, output_vector, _layers.size() - 1);
 	}
 
-	void MultilayerPerceptron::FeedForward( float* input_vector, float* output_vector, uint32_t layer_index)
+	void MultilayerPerceptron::FeedForward(float* input_vector, float* output_vector, uint32_t last_layer) const
 	{
-		assert(layer_index < _layers.size());
+		assert(last_layer < _layers.size());
 
-		memcpy(_activations[0], input_vector, sizeof(float) * _layers.front()->inputs);
-
-		for(uint32_t L = 0; L <= layer_index; L++)
+		_activations.front() = input_vector;
+		_activations.back() = output_vector;
+		for(size_t k = 0; k <= last_layer; k++)
 		{
-			const Layer& layer = *_layers[L];
-
-			float* input_activation_head = _activations[L];
-			float* output_accumulation_head = _accumulations[L+1];
-			float* output_activation_head = _activations[L+1];
-
-			const uint32_t input_blocks = BlockCount(layer.inputs);
-			for(uint32_t j = 0; j < layer.outputs; j++)
-			{
-				float* input_activation = input_activation_head;
-				float* weight_vector = layer.weights[j];
-
-				__m128 out_j = _mm_setzero_ps();
-				for(uint32_t i = 0; i < input_blocks; i++)
-				{
-					__m128 input = _mm_load_ps(input_activation);
-					__m128 w = _mm_load_ps(weight_vector);
-
-					// dot product
-					out_j = _mm_add_ps(out_j, _mm_mul_ps(input, w));
-
-					input_activation += 4;
-					weight_vector += 4;
-				}
-
-				// finish up our dot product
-				out_j = _mm_hadd_ps(out_j, out_j);
-				out_j = _mm_hadd_ps(out_j, out_j);
-
-				// bring it back
-				_mm_store_ss(output_accumulation_head + j, out_j);
-
-			}
-
-			// add in the biases and calc activation
-			const uint32_t output_blocks = BlockCount(layer.outputs);
-
-			float* output_activation = output_activation_head;
-			float* output_accumulation = output_accumulation_head;
-			float* biases = layer.biases;
-
-			// now calculate activation on all these buggers
-			
-			for(uint32_t j = 0; j < output_blocks; j++)
-			{
-				__m128 b = _mm_load_ps(biases);
-				// load our current accumulation
-				__m128 acc = _mm_load_ps(output_accumulation);
-				// add in the bias
-				acc = _mm_add_ps(acc, b);
-
-				__m128 act;
-				switch(layer.function)
-				{
-				case ActivationFunction::Linear:
-					act = acc;
-					break;
-				case ActivationFunction::RectifiedLinear:
-					act = _mm_rectifiedlinear_ps(acc);
-					break;
-				case ActivationFunction::Sigmoid:
-					act = _mm_sigmoid_ps(acc);
-					break;
-				}
-
-				_mm_store_ps(output_activation, act);
-
-				// move up our pointers
-				output_accumulation += 4;
-				output_activation += 4;
-				biases += 4;
-			}
-
-			// move this back to beginning of buffer
-			output_activation = output_activation_head;
-			// zero out the dangling bits here (left over memory from 16 byte padding)
-			// so we don't screw up next layer's dot products
-			for(uint32_t j = layer.outputs; j < output_blocks * 4; j++)
-			{
-				output_activation[j] = 0.0f;
-			}
+			_layers[k]->weights.CalcFeatureVector(_activations[k], _activations[k+1], _layers[k]->function);
 		}
-
-		// memcpy activation to the output buffer
-		memcpy(output_vector, _activations[layer_index + 1], sizeof(float) * _layers[layer_index]->outputs);
+		_activations.front() = nullptr;
+		_activations.back() = nullptr;
 	}
 
 	MultilayerPerceptron::Layer* MultilayerPerceptron::GetLayer( uint32_t index )
@@ -142,74 +58,23 @@ namespace OMLT
 		return _layers[index];
 	}
 
-	MultilayerPerceptron::Layer* MultilayerPerceptron::PopInputLayer()
-	{
-		assert(_layers.size() > 0);
-		
-		// first accumulation ptr is nullptr
-		_accumulations.erase(_accumulations.begin());
-		if(_layers.size() > 1)
-		{
-			_aligned_free(_accumulations.front());
-			_accumulations.front() = nullptr;
-
-		}		
-
-		_aligned_free(_activations.front());
-		_activations.erase(_activations.begin());
-
-		MLP::Layer* result = _layers.front();
-		_layers.erase(_layers.begin());
-
-		return result;
-	}
-
-	MultilayerPerceptron::Layer* MultilayerPerceptron::PopOutputLayer()
-	{
-		assert(_layers.size() > 0);
-
-		if(_layers.size() > 1)
-		{
-			_aligned_free(_accumulations.back());
-		}
-		_accumulations.erase(_accumulations.end() - 1);
-		
-		_aligned_free(_activations.back());
-		_activations.erase(_activations.end() - 1);
-
-		MLP::Layer* result = _layers.back();
-		_layers.erase(_layers.end() - 1);
-
-		return result;
-	}
-
 	bool MultilayerPerceptron::AddLayer( Layer* in_layer)
 	{
-		if(_layers.size() == 0)
+		if(_layers.size() > 0)
 		{
-			// we don't get the accumulations for input
-			_accumulations.push_back(nullptr);
-			
-			// we'll always copy in input vectors to the first slot, so give us some aligned memory here
+			if(_layers.back()->outputs != in_layer->inputs)
+			{
+				return false;
+			}
+
 			size_t input_alloc_size = sizeof(float) * BlockCount(in_layer->inputs) * 4;
-			_activations.push_back((float*)_aligned_malloc(input_alloc_size, 16));
+			
+			float* buffer = (float*)AlignedMalloc(input_alloc_size, 16);
+			memset(buffer, 0x00, input_alloc_size);
 
-			memset(_activations.front(), 0x00, input_alloc_size);
+			_activations.back() = buffer;
+			_activations.push_back(nullptr);
 		}
-		else if(_layers.back()->outputs != in_layer->inputs)
-		{
-			// unit count mismatch between existing top layer and new top layer
-			return false;
-		}
-
-		_layers.push_back(in_layer);
-		size_t alloc_size = sizeof(float) * (in_layer->outputs + 4);
-		_accumulations.push_back((float*)_aligned_malloc(alloc_size, 16));
-		_activations.push_back((float*)_aligned_malloc(alloc_size, 16));
-		
-		memset(_accumulations.back(), 0x00, alloc_size);
-		memset(_activations.back(), 0x00, alloc_size);
-		
 		return true;
 	}
 
@@ -228,7 +93,7 @@ namespace OMLT
 			cJSON_AddNumberToObject(layer, "Inputs", (*it)->inputs);			
 			cJSON_AddNumberToObject(layer, "Outputs", (*it)->outputs);
 			cJSON_AddStringToObject(layer, "Function", ActivationFunctionNames[(*it)->function]);
-			cJSON_AddItemToObject(layer, "Biases", cJSON_CreateFloatArray((*it)->biases, (*it)->outputs));
+			cJSON_AddItemToObject(layer, "Biases", cJSON_CreateFloatArray((*it)->weights.biases(), (*it)->outputs));
 			
 			cJSON* layer_weights = cJSON_CreateArray();
 			cJSON_AddItemToObject(layer, "Weights", layer_weights);
@@ -236,7 +101,7 @@ namespace OMLT
 			// now serialize out all those weights
 			for(uint32_t j = 0; j < (*it)->outputs; j++)
 			{
-				cJSON_AddItemToArray(layer_weights, cJSON_CreateFloatArray((*it)->weights[j], (*it)->inputs));
+				cJSON_AddItemToArray(layer_weights, cJSON_CreateFloatArray((*it)->weights.feature(j), (*it)->inputs));
 			}
 		}
 
@@ -309,7 +174,7 @@ namespace OMLT
 						{
 							// set this bias
 							cJSON_ArrayIteratorMoveNext(biases_it);
-							n_layer->biases[j] = (float)cJSON_ArrayIteratorCurrent(biases_it)->valuedouble;
+							n_layer->weights.biases()[j] = (float)cJSON_ArrayIteratorCurrent(biases_it)->valuedouble;
 							// and the weight vectors
 							cJSON_ArrayIteratorMoveNext(weights_it);
 							cJSON* w_j = cJSON_ArrayIteratorCurrent(weights_it);
@@ -319,7 +184,7 @@ namespace OMLT
 								for(uint32_t i = 0; i < n_layer->inputs; i++)
 								{
 									cJSON_ArrayIteratorMoveNext(w_j_it);
-									n_layer->weights[j][i] = (float)cJSON_ArrayIteratorCurrent(w_j_it)->valuedouble;
+									n_layer->weights.feature(j)[i] = (float)cJSON_ArrayIteratorCurrent(w_j_it)->valuedouble;
 								}
 								cJSON_Delete(w_j_it);
 							}
@@ -364,35 +229,6 @@ Malformed:
 		: inputs(in_inputs)
 		, outputs(in_outputs)
 		, function(in_function)
-		, biases(nullptr)
-		, weights(nullptr)
-	{
-
-		// allocate space for biases
-		const uint32_t biases_alloc_size = sizeof(float) * BlockCount(outputs) * 4;
-		biases = (float*)_aligned_malloc(biases_alloc_size, 16);
-		memset(biases, 0x00, biases_alloc_size);
-
-		
-		// allocate space for weights
-		weights = new float*[outputs];
-
-		const uint32_t weight_alloc_size = sizeof(float) * BlockCount(inputs) * 4;
-		for(uint32_t j = 0; j < outputs; j++)
-		{
-			weights[j] = (float*)_aligned_malloc(weight_alloc_size, 16);
-			memset(weights[j], 0x00, weight_alloc_size);
-		}
-	}
-
-	MultilayerPerceptron::Layer::~Layer()
-	{
-		_aligned_free(biases);
-
-		for(uint32_t j = 0; j < outputs; j++)
-		{
-			_aligned_free(weights[j]);
-		}
-		delete[] weights;
-	}
+		, weights(inputs, outputs)
+	{ }
 }
